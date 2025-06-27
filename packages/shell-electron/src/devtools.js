0b806a18ea98773ee953383b6948e719312f734f
dingxinwei <57656377+dingxinwei@users.noreply.github.com>
2025-06-27 15:05:02 +0800

    Update devtools.js

--- packages/shell-electron/src/devtools.js
+++ packages/shell-electron/src/devtools.js
@@ -1,15 +1,37 @@
 import io from 'socket.io-client'
 import { initDevTools } from '@front'
 import { Bridge } from '@vue-devtools/shared-utils'
-
-const port = window.process.env.PORT || 8098
-const socket = io(`http://localhost:${port}`)
+const roomId = getRouterParam('roomId')
+const origin = window.location.origin
+const socket = io(origin + `?roomId=${roomId}`, { transports: ['websocket'], path: '/universal/vueDevtools/websocket/' })
 const $ = document.querySelector.bind(document)
 const $intro = $('#intro')
 
 let reload = null
 let introTimer
-
+function getRouterParam (name) {
+  const params = window.location.href.split('?')[1]
+  if (!params) {
+    return ''
+  }
+  let paramList = []
+  let param = null
+  const theRequest = {}
+  if (params.length > 0) {
+    if (params.indexOf('&') >= 0) {
+      paramList = params.split('&')
+    } else {
+      paramList[0] = params
+    }
+    for (let i = 0; i < paramList.length; i++) {
+      theRequest[paramList[i].split('=')[0]] = decodeURIComponent(
+        paramList[i].split('=')[1]
+      )
+    }
+    param = theRequest[name]
+  }
+  return param
+}
 socket.on('vue-devtools-disconnect-devtools', () => {
   introTimer = setTimeout(() => {
     $intro.classList.remove('hidden')
@@ -24,17 +46,15 @@ socket.on('vue-devtools-init', () => {
   socket.off('vue-message')
 
   // If new page is opened reload devtools
-  if (reload) {
-    return reload()
-  }
+  if (reload) return reload()
 
   initDevTools({
-    connect(callback) {
+    connect (callback) {
       const wall = {
-        listen(fn) {
+        listen (fn) {
           socket.on('vue-message', data => fn(data))
         },
-        send(data) {
+        send (data) {
           if (process.env.NODE_ENV !== 'production') {
             // eslint-disable-next-line no-console
             console.log('%cdevtools -> backend', 'color:#888;', data)
@@ -46,7 +66,7 @@ socket.on('vue-devtools-init', () => {
 
       callback(bridge)
     },
-    onReload(fn) {
+    onReload (fn) {
       reload = fn
     },
   })
